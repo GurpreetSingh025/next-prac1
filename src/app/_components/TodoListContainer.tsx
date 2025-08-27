89245a41758dbf973080e3a6109dde73777f530a
GurpreetSingh025 <princegsd09@gmail.com>
2025-08-28 02:05:48 +0530

    hard coded UI done

--- src/app/_components/TodoListContainer.tsx
+++ src/app/_components/TodoListContainer.tsx
@@ -1,7 +1,37 @@
+import styles from '../_styles/todoListContainer.module.css'
+import { MdOutlineEdit , MdOutlineDelete  } from "react-icons/md";
+
 export default function TodoListContainer(){
      return(
-          <div>
-            
+          <div className={`${styles.flex} ${styles.todoListContainer}`}>
+             <div className={`${styles.listItem} ${styles.flex}`}>                 
+                <div className={`${styles.flex} ${styles.itemDetails}`}><input type='checkbox' /><p>Learn Next.js 16</p></div>
+                <div className={`${styles.flex} ${styles.itemOperations}`}><MdOutlineEdit className={`${styles.iconStyle}`}/><MdOutlineDelete className={`${styles.iconStyle}`}/></div>
+             </div>
+            <div className={`${styles.listItem} ${styles.flex}`}>                 
+                <div className={`${styles.flex} ${styles.itemDetails}`}><input type='checkbox' /><p>Learn Next.js 16</p></div>
+                <div className={`${styles.flex} ${styles.itemOperations}`}><MdOutlineEdit className={`${styles.iconStyle}`}/><MdOutlineDelete className={`${styles.iconStyle}`}/></div>
+             </div>
+                         <div className={`${styles.listItem} ${styles.flex}`}>                 
+                <div className={`${styles.flex} ${styles.itemDetails}`}><input type='checkbox' /><p>Learn Next.js 16</p></div>
+                <div className={`${styles.flex} ${styles.itemOperations}`}><MdOutlineEdit className={`${styles.iconStyle}`}/><MdOutlineDelete className={`${styles.iconStyle}`}/></div>
+             </div>
+                                      <div className={`${styles.listItem} ${styles.flex}`}>                 
+                <div className={`${styles.flex} ${styles.itemDetails}`}><input type='checkbox' /><p>Learn Next.js 16</p></div>
+                <div className={`${styles.flex} ${styles.itemOperations}`}><MdOutlineEdit className={`${styles.iconStyle}`}/><MdOutlineDelete className={`${styles.iconStyle}`}/></div>
+             </div>                         <div className={`${styles.listItem} ${styles.flex}`}>                 
+                <div className={`${styles.flex} ${styles.itemDetails}`}><input type='checkbox' /><p>Learn Next.js 16</p></div>
+                <div className={`${styles.flex} ${styles.itemOperations}`}><MdOutlineEdit className={`${styles.iconStyle}`}/><MdOutlineDelete className={`${styles.iconStyle}`}/></div>
+             </div>                         <div className={`${styles.listItem} ${styles.flex}`}>                 
+                <div className={`${styles.flex} ${styles.itemDetails}`}><input type='checkbox' /><p>Learn Next.js 16</p></div>
+                <div className={`${styles.flex} ${styles.itemOperations}`}><MdOutlineEdit className={`${styles.iconStyle}`}/><MdOutlineDelete className={`${styles.iconStyle}`}/></div>
+             </div>                         <div className={`${styles.listItem} ${styles.flex}`}>                 
+                <div className={`${styles.flex} ${styles.itemDetails}`}><input type='checkbox' /><p>Learn Next.js 16</p></div>
+                <div className={`${styles.flex} ${styles.itemOperations}`}><MdOutlineEdit className={`${styles.iconStyle}`}/><MdOutlineDelete className={`${styles.iconStyle}`}/></div>
+             </div>                         <div className={`${styles.listItem} ${styles.flex}`}>                 
+                <div className={`${styles.flex} ${styles.itemDetails}`}><input type='checkbox' /><p>Learn Next.js 16</p></div>
+                <div className={`${styles.flex} ${styles.itemOperations}`}><MdOutlineEdit className={`${styles.iconStyle}`}/><MdOutlineDelete className={`${styles.iconStyle}`}/></div>
+             </div>
           </div>
      )
 }
